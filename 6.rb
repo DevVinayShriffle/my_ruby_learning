6ed38f1dbc63caaba3545cc29606c593dc00d49b
DevVinayShriffle <vinayks@shriffle.com>
2026-01-12 14:12:29 +0530

    Status File

--- 6.rb
+++ 6.rb
@@ -1,52 +1,88 @@
-# str="Vinaykumarsahu"
-# newstr="Vinay \n Kumar \n Sahu"
+# # str="Vinaykumarsahu"
+# # newstr="Vinay \n Kumar \n Sahu"
 
-# puts str.hash
-# puts str
-# puts newstr.hex
-
-
+# # puts str.hash
+# # puts str
+# # puts newstr.hex
 
 
-# puts "abc".hex
 
 
-# a = "abc"
-# p a.hex
+# # puts "abc".hex
 
-# 0-9
-# a-f
-# A-F
 
-# str="Vinay Kumar Sahu"
-# puts str.include? "9"
+# # a = "abc"
+# # p a.hex
 
-# str="9 Vinay Kumar Sahu"
-# puts str.insert(6 , "*#&$")
-# puts str.inspect
+# # 0-9
+# # a-f
+# # A-F
 
-# puts str.length
-# puts str.ljust(25 , "abc")
-# puts str.lstrip!
-# puts str.match(/ /)
+# # str="Vinay Kumar Sahu"
+# # puts str.include? "9"
 
-# str = "Name: John Doe, ID: 12345"
-# pattern = /ID: (\d+)/
+# # str="9 Vinay Kumar Sahu"
+# # puts str.insert(6 , "*#&$")
+# # puts str.inspect
 
-# puts str.match(pattern)
-# puts str.oct
+# # puts str.length
+# # puts str.ljust(25 , "abc")
+# # puts str.lstrip!
+# # puts str.match(/ /)
 
-# puts str.replace("Lav kush sahu")
-# puts str.reverse
-# puts str.reverse!
-# puts str.rindex("a",10)
-# puts str.rjust(50, padstr="abc")
+# # str = "Name: John Doe, ID: 12345"
+# # pattern = /ID: (\d+)/
 
-# str="   Vinay Kumar Sahu   "
-# puts str.rstrip
-# puts str.rstrip!
+# # puts str.match(pattern)
+# # puts str.oct
 
-str="Vinay Kumar Sahu"
-puts str.scan("a").class
-# puts str.include?(" ")
-puts str
+# # puts str.replace("Lav kush sahu")
+# # puts str.reverse
+# # puts str.reverse!
+# # puts str.rindex("a",10)
+# # puts str.rjust(50, padstr="abc")
+
+# # str="   Vinay Kumar Sahu   "
+# # puts str.rstrip
+# # puts str.rstrip!
+
+# # str="Vinay Kumar Sahu"
+# # puts str.scan("a").class
+# # # puts str.include?(" ")
+# # puts str
+
+# # name = Hash["name"=>"Vinay","age"=>23,"Degree"=>"B.tech"]
+# # name = {"name"=>"Vinay","age"=>23,"Degree"=>"B.tech"}
+# # puts name.include?("age")
+# # puts name.to_a.to_s.class
+# # puts name.values_at("name","Degree")
+# # # :name = "Sahu"
+# # user = { name: "Alice", age: 30 }
+# # puts user[:age]
+# # NAME = "vINAY"
+# # puts NAME
+# # NAME = "sAHU"
+# # puts NAME
+# (1..5).each do |i|
+# 	puts i 
+# end
+
+# col=(1..5).collect {|i| i*2}
+# puts col
+
+
+class Name 
+	def initialize (fname, lname)
+		@fname = fname
+		@lname = lname
+	end
+
+	def print
+		puts @lname
+		puts @fname
+	end
+end
+
+name = Name.new("Vinay", "Kumar")
+name.print
+
---
+++ status.rb
@@ -0,0 +1,16 @@
+12/01/2026
+iterators (Enumerators) = inject, reduce, enumarators
+modifier => Inline
+Precedence of operator
+Unless...else like if...else
+Normal apply => a&&data
+Name Convention => file = snake_case
+				    module class = Capitalize
+				    operator = Bitwise, defined
+? => true/false ==> Boolean Method (present,include etc.)
+! => Update data ==> Update Method (save! etc.)
+Method => instance method
+		  class method => self method -> Ruby doc
+Slack Installation
+Multiple Inheritance -> Mixins -> Types = 3 ==> include, extend, prepend
+git add remote (origin is here remote., Origin can be multiple.)
