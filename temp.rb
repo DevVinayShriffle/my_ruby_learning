461579e1527d676c04cee6c3ae4311f32ecdb82f
DevVinayShriffle <vinayks@shriffle.com>
2026-01-14 12:11:23 +0530

    commiting

--- temp.rb
+++ temp.rb
@@ -1,3 +1,19 @@
-puts "Hello World!, How are you? what are you doing today?"
-puts "Second line"
-puts "Third line"
+# puts "Hello World!, How are you? what are you doing today?"
+# puts "Second line"
+# puts "Third line"
+
+class Temp
+	def set_name (name)
+		@@name = name
+	end
+
+	def print
+		puts @@name
+	end
+end
+
+t1 = Temp.new
+t2 = Temp.new
+
+t1.set_name("Vinay")
+t2.print
